e82a0c442ccb77dcd34f58c925d06a8fb85cc89a
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-01-07 16:32:23 +0900

    Docs: intial setting

---
+++ src/test/java/com/example/dgbackend/DgBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
