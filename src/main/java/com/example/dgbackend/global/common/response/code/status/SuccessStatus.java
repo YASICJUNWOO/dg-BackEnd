8c3d3b4f3e556b2115e54522ddfdab2e9ac8f411
Minhyeok Song <minhyeok.dev@gmail.com>
2024-01-11 19:42:49 +0900

    Feat: 공통 응답 설정
    Related: #5

---
+++ src/main/java/com/example/dgbackend/global/common/response/code/status/SuccessStatus.java
@@ -0,0 +1,43 @@
+package com.example.dgbackend.global.common.response.code.status;
+
+import com.example.dgbackend.global.common.response.code.BaseCode;
+import com.example.dgbackend.global.common.response.code.ReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    // 일반적인 응답
+    _OK(HttpStatus.OK, "COMMON200", "성공입니다.");
+
+    // 멤버 관련 응답
+
+    // ~~~ 관련 응답
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+            .message(message)
+            .code(code)
+            .isSuccess(true)
+            .build();
+    }
+
+    @Override
+    public ReasonDto getReasonHttpStatus() {
+        return ReasonDto.builder()
+            .message(message)
+            .code(code)
+            .isSuccess(true)
+            .httpStatus(httpStatus)
+            .build()
+            ;
+    }
+}
