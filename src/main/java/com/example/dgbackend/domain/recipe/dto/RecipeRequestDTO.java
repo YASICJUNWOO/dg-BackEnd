c6dadfecac1617eace8eb4287af224cf32eedde7
YASICJUNWOO <joonoo3@naver.com>
2024-01-18 01:29:43 +0900

    feat : Recipe DTO 추가
    Related #12

---
+++ src/main/java/com/example/dgbackend/domain/recipe/dto/RecipeRequestDTO.java
@@ -0,0 +1,35 @@
+package com.example.dgbackend.domain.recipe.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecipeRequestDTO {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String info;
+
+    @NotNull
+    private String cookingTime;
+
+    @NotNull
+    private String calorie;
+
+    @NotNull
+    private String ingredient;
+
+    @NotNull
+    private String recipeInstruction;
+
+    private String recommendCombination;
+
+}
---
+++ src/main/java/com/example/dgbackend/domain/recipe/dto/RecipeResponseDTO.java
@@ -0,0 +1,46 @@
+package com.example.dgbackend.domain.recipe.dto;
+
+import com.example.dgbackend.domain.member.Member;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecipeResponseDTO {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String info;
+
+    @NotNull
+    private String cookingTime;
+
+    @NotNull
+    private String calorie;
+
+    @NotNull
+    private Long likeCount;
+
+    @NotNull
+    private Long commentCount;
+
+    @NotNull
+    private String ingredient;
+
+    @NotNull
+    private String recipeInstruction;
+
+    private String recommendCombination; //추천받은 조합
+
+    private boolean state = true; //true : 존재, false : 삭제
+
+    private String memberName;
+
+}
