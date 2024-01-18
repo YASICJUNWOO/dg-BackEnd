904df114ebb0a574374aef5c6ad392624accc555
YASICJUNWOO <joonoo3@naver.com>
2024-01-18 19:54:00 +0900

    refactor : impl 형식 추가
    Related #12

--- src/main/java/com/example/dgbackend/domain/recipe/service/RecipeService.java
+++ src/main/java/com/example/dgbackend/domain/recipe/service/RecipeService.java
@@ -1,89 +1,31 @@
 package com.example.dgbackend.domain.recipe.service;
 
 import com.example.dgbackend.domain.member.Member;
-import com.example.dgbackend.domain.member.service.MemberService;
 import com.example.dgbackend.domain.recipe.Recipe;
-import com.example.dgbackend.domain.recipe.converter.RecipeConverter;
 import com.example.dgbackend.domain.recipe.dto.RecipeParamVO;
 import com.example.dgbackend.domain.recipe.dto.RecipeRequestDTO;
 import com.example.dgbackend.domain.recipe.dto.RecipeResponseDTO;
-import com.example.dgbackend.domain.recipe.repository.RecipeRepository;
-import com.example.dgbackend.global.common.response.code.status.ErrorStatus;
-import com.example.dgbackend.global.exception.ApiException;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class RecipeService {
+public interface RecipeService {
 
-    private final RecipeRepository recipeRepository;
-    private final MemberService memberService;
+    List<RecipeResponseDTO> getExistRecipes();
 
-    @Transactional
-    public List<RecipeResponseDTO> getExistRecipes() {
-        return recipeRepository.findAllByState(true).stream()
-                .map(RecipeConverter::toResponse)
-                .toList();
-    }
+    RecipeResponseDTO getRecipeDetail(RecipeParamVO recipeParamVO);
 
-    @Transactional
-    public RecipeResponseDTO getRecipeDetail(RecipeParamVO recipeParamVO) {
-        Recipe recipe = getRecipe(recipeParamVO);
-        return RecipeConverter.toResponse(recipe);
-    }
+    RecipeResponseDTO createRecipe(RecipeRequestDTO recipeRequestDto, Member member);
 
-    @Transactional
-    public RecipeResponseDTO createRecipe(RecipeRequestDTO recipeRequestDto, Member member) {
+    RecipeResponseDTO updateRecipe(RecipeParamVO recipeParamVO, RecipeRequestDTO recipeRequestDto);
 
-        Member memberEntity = memberService.findMemberByName(member.getName());
-        isAlreadyCreate(recipeRequestDto.getName(),memberEntity.getName());
-        return RecipeConverter.toResponse(recipeRepository.save(RecipeConverter.toEntity(recipeRequestDto, memberEntity)));
-    }
-
-    @Transactional
-    public RecipeResponseDTO updateRecipe(RecipeParamVO recipeParamVO, RecipeRequestDTO recipeRequestDto) {
-        Recipe recipe = getRecipe(recipeParamVO).update(recipeRequestDto);
-        isAlreadyCreate(recipe.getName(),recipe.getMember().getName());
-        return RecipeConverter.toResponse(recipeRepository.save(recipe));
-    }
-
-    @Transactional
-    public void deleteRecipe(RecipeParamVO recipeParamVO) {
-        Recipe recipe = getRecipe(recipeParamVO).delete();
-        recipeRepository.save(recipe);
-    }
+    void deleteRecipe(RecipeParamVO recipeParamVO);
 
     //레시피 이름과 회원 이름으로 레시피 탐색
-    @Transactional
-    public Recipe getRecipe(RecipeParamVO recipeParamVO) {
-
-        Recipe recipe = recipeRepository.findByNameAndMember_Name(recipeParamVO.getName(), recipeParamVO.getMemberName())
-                .orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE));
-
-        return isDelete(recipe);
-    }
+    Recipe getRecipe(RecipeParamVO recipeParamVO);
 
     //레시피가 삭제된 경우 예외처리
-    @Transactional
-    public Recipe isDelete(Recipe recipe) {
+    Recipe isDelete(Recipe recipe);
 
-        if (!recipe.isState()) throw new ApiException(ErrorStatus._DELETE_RECIPE);
-
-        return recipe;
-    }
-
-    @Transactional
-    public void isAlreadyCreate(String RecipeName, String memberName) {
-        recipeRepository.findByNameAndMember_Name(RecipeName, memberName)
-                .ifPresent(recipe -> {
-                    throw new ApiException(ErrorStatus._ALREADY_CREATE_RECIPE);
-                });
-    }
+    void isAlreadyCreate(String RecipeName, String memberName);
 
 }
