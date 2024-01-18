366173387480f1f288ede1ad6323dc33d53bd526
YASICJUNWOO <joonoo3@naver.com>
2024-01-18 19:55:46 +0900

    refactor : Transactional 수정
    꼭 필요한 메서드에만 붙였습니다!
    Related #12

---
+++ src/main/java/com/example/dgbackend/domain/recipe/service/RecipeServiceImpl.java
@@ -0,0 +1,91 @@
+package com.example.dgbackend.domain.recipe.service;
+
+import com.example.dgbackend.domain.member.Member;
+import com.example.dgbackend.domain.member.service.MemberService;
+import com.example.dgbackend.domain.recipe.Recipe;
+import com.example.dgbackend.domain.recipe.converter.RecipeConverter;
+import com.example.dgbackend.domain.recipe.dto.RecipeParamVO;
+import com.example.dgbackend.domain.recipe.dto.RecipeRequestDTO;
+import com.example.dgbackend.domain.recipe.dto.RecipeResponseDTO;
+import com.example.dgbackend.domain.recipe.repository.RecipeRepository;
+import com.example.dgbackend.global.common.response.code.status.ErrorStatus;
+import com.example.dgbackend.global.exception.ApiException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+    private final MemberService memberService;
+
+    @Override
+    public List<RecipeResponseDTO> getExistRecipes() {
+        return recipeRepository.findAllByState(true).stream()
+                .map(RecipeConverter::toResponse)
+                .toList();
+    }
+
+    @Override
+    public RecipeResponseDTO getRecipeDetail(RecipeParamVO recipeParamVO) {
+        Recipe recipe = getRecipe(recipeParamVO);
+        return RecipeConverter.toResponse(recipe);
+    }
+
+    @Override
+    public RecipeResponseDTO createRecipe(RecipeRequestDTO recipeRequestDto, Member member) {
+
+        Member memberEntity = memberService.findMemberByName(member.getName());
+        isAlreadyCreate(recipeRequestDto.getName(),memberEntity.getName());
+        return RecipeConverter.toResponse(recipeRepository.save(RecipeConverter.toEntity(recipeRequestDto, memberEntity)));
+    }
+
+    @Override
+    @Transactional
+    public RecipeResponseDTO updateRecipe(RecipeParamVO recipeParamVO, RecipeRequestDTO recipeRequestDto) {
+        Recipe recipe = getRecipe(recipeParamVO).update(recipeRequestDto);
+        isAlreadyCreate(recipe.getName(),recipe.getMember().getName());
+        return RecipeConverter.toResponse(recipeRepository.save(recipe));
+    }
+
+    @Override
+    @Transactional
+    public void deleteRecipe(RecipeParamVO recipeParamVO) {
+        Recipe recipe = getRecipe(recipeParamVO).delete();
+        recipeRepository.save(recipe);
+    }
+
+    //레시피 이름과 회원 이름으로 레시피 탐색
+    @Override
+    public Recipe getRecipe(RecipeParamVO recipeParamVO) {
+
+        Recipe recipe = recipeRepository.findByNameAndMember_Name(recipeParamVO.getName(), recipeParamVO.getMemberName())
+                .orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE));
+
+        return isDelete(recipe);
+    }
+
+    //레시피가 삭제된 경우 예외처리
+    @Override
+    public Recipe isDelete(Recipe recipe) {
+
+        if (!recipe.isState()) throw new ApiException(ErrorStatus._DELETE_RECIPE);
+
+        return recipe;
+    }
+
+    @Override
+    public void isAlreadyCreate(String RecipeName, String memberName) {
+        recipeRepository.findByNameAndMember_Name(RecipeName, memberName)
+                .ifPresent(recipe -> {
+                    throw new ApiException(ErrorStatus._ALREADY_CREATE_RECIPE);
+                });
+    }
+
+}
