ea64f95aa297a7f0d9e532c4d58ee427741a9d87
YASICJUNWOO <joonoo3@naver.com>
2024-01-18 14:13:22 +0900

    feat : 레시피 이름과 작성자로 레시피 조회 메서드
    - 식별자인 레시피 이름과 작성자 이름으로 조회하고 없을 시 예외를 반환하도록 했습니다.
    - 조회 성공시 삭제 여부 메소드까지 확인하도록 했습니다.
    Related #12

--- src/main/java/com/example/dgbackend/domain/recipe/service/RecipeService.java
+++ src/main/java/com/example/dgbackend/domain/recipe/service/RecipeService.java
@@ -2,6 +2,7 @@ package com.example.dgbackend.domain.recipe.service;
 
 import com.example.dgbackend.domain.recipe.Recipe;
 import com.example.dgbackend.domain.recipe.converter.RecipeConverter;
+import com.example.dgbackend.domain.recipe.dto.RecipeParamVO;
 import com.example.dgbackend.domain.recipe.dto.RecipeResponseDTO;
 import com.example.dgbackend.domain.recipe.repository.RecipeRepository;
 import com.example.dgbackend.global.common.response.code.status.ErrorStatus;
@@ -25,6 +26,16 @@ public class RecipeService {
         return recipeRepository.findAllByState(true).stream()
                 .map(RecipeConverter::toResponse)
                 .toList();
+    }
+
+    //레시피 이름과 회원 이름으로 레시피 탐색
+    @Transactional
+    public Recipe getRecipe(RecipeParamVO recipeParamVO) {
+
+        Recipe recipe = recipeRepository.findByNameAndMember_Name(recipeParamVO.getName(), recipeParamVO.getMemberName())
+                .orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE));
+
+        return isDelete(recipe);
     }
 
     //레시피가 삭제된 경우 예외처리
