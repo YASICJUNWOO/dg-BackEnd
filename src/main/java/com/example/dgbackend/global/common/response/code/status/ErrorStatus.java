cb03575af8b9af6c1382661a4007a7695e29bd19
YASICJUNWOO <joonoo3@naver.com>
2024-01-18 14:03:57 +0900

    feat :  없는 Recipe 접근 시 예외 추가
    Related #12

--- src/main/java/com/example/dgbackend/global/common/response/code/status/ErrorStatus.java
+++ src/main/java/com/example/dgbackend/global/common/response/code/status/ErrorStatus.java
@@ -20,7 +20,11 @@ public enum ErrorStatus implements BaseErrorCode {
 
     //인증 관련
     _EMPTY_JWT(HttpStatus.UNAUTHORIZED, "AUTH_001", "JWT가 존재하지 않습니다."),
-    _INVALID_JWT(HttpStatus.UNAUTHORIZED, "AUTH_002", "유효하지 않은 JWT입니다.");
+    _INVALID_JWT(HttpStatus.UNAUTHORIZED, "AUTH_002", "유효하지 않은 JWT입니다."),
+
+    //레시피
+    _EMPTY_RECIPE(HttpStatus.CONFLICT, "RECIPE_001", "존재하지 않는 레시피입니다."),
+    _DELETE_RECIPE(HttpStatus.BAD_REQUEST, "RECIPE_002", "삭제된 레시피입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
