5909e29c38b3d8fa7093f7fe56f803d8453f2b5a
YASICJUNWOO <joonoo3@naver.com>
2024-01-18 14:19:38 +0900

    feat : RecipeRequestDTO -> Recipe 변환
    likeCount와 commentCount는 생성시 0으로 빌드하였습니다.
    Related #12

--- src/main/java/com/example/dgbackend/domain/recipe/converter/RecipeConverter.java
+++ src/main/java/com/example/dgbackend/domain/recipe/converter/RecipeConverter.java
@@ -1,6 +1,8 @@
 package com.example.dgbackend.domain.recipe.converter;
 
+import com.example.dgbackend.domain.member.Member;
 import com.example.dgbackend.domain.recipe.Recipe;
+import com.example.dgbackend.domain.recipe.dto.RecipeRequestDTO;
 import com.example.dgbackend.domain.recipe.dto.RecipeResponseDTO;
 
 public class RecipeConverter {
@@ -18,6 +20,21 @@ public class RecipeConverter {
                 .recommendCombination(recipe.getRecommendCombination())
                 .state(recipe.isState())
                 .memberName(recipe.getMember().getName())
+                .build();
+    }
+
+    public static Recipe toEntity(RecipeRequestDTO recipeRequestDto, Member member) {
+        return Recipe.builder()
+                .name(recipeRequestDto.getName())
+                .info(recipeRequestDto.getInfo())
+                .cookingTime(recipeRequestDto.getCookingTime())
+                .calorie(recipeRequestDto.getCalorie())
+                .likeCount(0L)
+                .commentCount(0L)
+                .ingredient(recipeRequestDto.getIngredient())
+                .recipeInstruction(recipeRequestDto.getRecipeInstruction())
+                .recommendCombination(recipeRequestDto.getRecommendCombination())
+                .member(member)
                 .build();
     }
 
