e327ff800aa5c425e695cc5f62017d9afc14015b
YASICJUNWOO <joonoo3@naver.com>
2024-01-19 23:01:07 +0900

    feat : 조건에 따라 RecipeComment Entity 생성해 반환
    - parentId가 0이라면 parentComment를 null로 생성합니다.
    - parentId가 0이 아니라면 parentRecipeComment를 찾아 반환받고 Entity를 생성합니다.
    Related #33

--- src/main/java/com/example/dgbackend/domain/recipecomment/service/RecipeCommentService.java
+++ src/main/java/com/example/dgbackend/domain/recipecomment/service/RecipeCommentService.java
@@ -1,8 +1,10 @@
 package com.example.dgbackend.domain.recipecomment.service;
 
 import com.example.dgbackend.domain.recipecomment.RecipeComment;
+import com.example.dgbackend.domain.recipecomment.dto.RecipeCommentVO;
 
 public interface RecipeCommentService {
+    RecipeComment getEntity(RecipeCommentVO paramVO);
 
     RecipeComment getParentEntityById(Long id);
 
--- src/main/java/com/example/dgbackend/domain/recipecomment/service/RecipeCommentServiceImpl.java
+++ src/main/java/com/example/dgbackend/domain/recipecomment/service/RecipeCommentServiceImpl.java
@@ -1,17 +1,43 @@
 package com.example.dgbackend.domain.recipecomment.service;
 
+import com.example.dgbackend.domain.member.Member;
+import com.example.dgbackend.domain.member.service.MemberService;
+import com.example.dgbackend.domain.recipe.Recipe;
+import com.example.dgbackend.domain.recipe.service.RecipeService;
 import com.example.dgbackend.domain.recipecomment.RecipeComment;
+import com.example.dgbackend.domain.recipecomment.dto.RecipeCommentRequest;
+import com.example.dgbackend.domain.recipecomment.dto.RecipeCommentVO;
 import com.example.dgbackend.domain.recipecomment.repository.RecipeCommentRepository;
 import com.example.dgbackend.global.common.response.code.status.ErrorStatus;
 import com.example.dgbackend.global.exception.ApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class RecipeCommentServiceImpl implements RecipeCommentService {
 
     private final RecipeCommentRepository recipeCommentRepository;
+    private final MemberService memberService;
+    private final RecipeService recipeService;
+
+    @Override
+    public RecipeComment getEntity(RecipeCommentVO paramVO) {
+        Member member = memberService.findMemberByName(paramVO.getMemberName());
+        Recipe recipe = recipeService.getRecipe(paramVO.getRecipeId());
+        String content = paramVO.getContent();
+
+        return Optional.ofNullable(paramVO.getParentId())
+
+                //대댓글 일 때
+                .filter(parentId -> parentId != 0)
+                .map(parentId -> RecipeCommentRequest.toEntity(member, recipe, content, getParentEntityById(parentId)))
+
+                //댓글 일 때
+                .orElse(RecipeCommentRequest.toEntity(member, recipe, content, null));
+    }
 
     @Override
     public RecipeComment getParentEntityById(Long parentId) {
