1e3bf8a9577adbea5a19fc07af2015f40c0f6faa
YASICJUNWOO <joonoo3@naver.com>
2024-01-20 00:28:42 +0900

    refactor : 삭제된 Comment는 반환하지 않음
    - 부모 댓글 조회 시 삭제되지 않은 것만 조회
    - 대댓글 조회시 부모가 삭제되지않음 & 자기 자신이 삭제되지 않은 것만 조회
    Related #33

--- src/main/java/com/example/dgbackend/domain/recipecomment/dto/RecipeCommentResponse.java
+++ src/main/java/com/example/dgbackend/domain/recipecomment/dto/RecipeCommentResponse.java
@@ -25,22 +25,19 @@ public class RecipeCommentResponse {
 
     private String MemberName;
 
-    private boolean state;
-
     @Builder.Default
     private List<RecipeCommentResponse> childCommentList = new ArrayList<>();
 
     public static RecipeCommentResponse toResponse(RecipeComment recipeComment) {
 
         //부모 Comment가 null이면 0으로 설정, null이 아니면 부모 Comment의 id값으로 설정
-        Long parentId = Optional.of(recipeComment.getParentComment())
+        Long parentId = Optional.ofNullable(recipeComment.getParentComment())
                 .map(RecipeComment::getId)
                 .orElse(0L);
 
         return RecipeCommentResponse.builder()
                 .id(recipeComment.getId())
                 .content(recipeComment.getContent())
-                .state(recipeComment.isState())
                 .childCommentList(getList(recipeComment))
                 .MemberName(recipeComment.getMember().getName())
                 .build();
@@ -48,8 +45,14 @@ public class RecipeCommentResponse {
 
     private static List<RecipeCommentResponse> getList(RecipeComment recipeComment) {
         return Optional.ofNullable(recipeComment.getChildCommentList())
+
+                //자식없을 때
                 .orElse(new ArrayList<>())
-                .stream().map(RecipeCommentResponse::toResponse).toList();
+
+                //자식있을 때, 존재하는 것 만 반환
+                .stream()
+                .filter(RecipeComment::isState)
+                .map(RecipeCommentResponse::toResponse).toList();
     }
 
 }
