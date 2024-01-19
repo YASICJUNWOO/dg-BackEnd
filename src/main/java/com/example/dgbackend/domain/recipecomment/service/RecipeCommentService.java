38acd77cceead83c26c24d5590caf52f6f8a6973
YASICJUNWOO <joonoo3@naver.com>
2024-01-19 22:53:29 +0900

    feat : 부모 RecipeComment 조회 & 예외처리
    - 부모의 RecipeComment를 조회하여 가져옵니다.
    - 부모 RecipeComment가 부모 RecipeComment 갖는다는 것은 해당 Commment가 대댓글 이상이라는 의미이므로 예외처리 해줍니다.
    Related #33

--- src/main/java/com/example/dgbackend/domain/recipecomment/service/RecipeCommentService.java
+++ src/main/java/com/example/dgbackend/domain/recipecomment/service/RecipeCommentService.java
@@ -3,6 +3,9 @@ package com.example.dgbackend.domain.recipecomment.service;
 import com.example.dgbackend.domain.recipecomment.RecipeComment;
 
 public interface RecipeCommentService {
+
+    RecipeComment getParentEntityById(Long id);
+
     RecipeComment getEntityById(Long id);
 
 }
