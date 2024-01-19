a4863897448e8200336f31aef220b2db2043954b
YASICJUNWOO <joonoo3@naver.com>
2024-01-19 22:48:26 +0900

    feat : id로 RecipeComment 조회 & 예외처리

---
+++ src/main/java/com/example/dgbackend/domain/recipecomment/service/RecipeCommentService.java
@@ -0,0 +1,8 @@
+package com.example.dgbackend.domain.recipecomment.service;
+
+import com.example.dgbackend.domain.recipecomment.RecipeComment;
+
+public interface RecipeCommentService {
+    RecipeComment getEntityById(Long id);
+
+}
