3dada359288ecba3b05106af20a452ecd84b263b
YASICJUNWOO <joonoo3@naver.com>
2024-01-29 03:08:09 +0900

    feat : 요청으로 들어온 Image가 없을 경우 예외처리
    Related #53

--- src/main/java/com/example/dgbackend/domain/recipeimage/service/RecipeImageService.java
+++ src/main/java/com/example/dgbackend/domain/recipeimage/service/RecipeImageService.java
@@ -1,12 +1,22 @@
 package com.example.dgbackend.domain.recipeimage.service;
 
+import com.example.dgbackend.domain.recipe.Recipe;
+import com.example.dgbackend.domain.recipe.dto.RecipeResponse;
 import com.example.dgbackend.domain.recipe.service.RecipeService;
 import com.example.dgbackend.domain.recipeimage.RecipeImage;
+import com.example.dgbackend.domain.recipeimage.dto.RecipeImageRequest;
+import com.example.dgbackend.domain.recipeimage.dto.RecipeImageResponse;
+import com.example.dgbackend.domain.recipeimage.dto.RecipeImageVO;
 import com.example.dgbackend.domain.recipeimage.repository.RecipeImageRepository;
+import com.example.dgbackend.global.common.response.code.status.ErrorStatus;
+import com.example.dgbackend.global.exception.ApiException;
 import com.example.dgbackend.global.s3.S3Service;
+import com.example.dgbackend.global.s3.dto.S3Result;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -24,6 +34,16 @@ public class RecipeImageService {
         return recipeImageRepository.findAllByRecipeId(recipeId).stream()
                 .map(RecipeImage::getImageUrl)
                 .toList();
+    }
+
+    //파일 없을 시 예외처리
+    private List<MultipartFile> validFileList(List<MultipartFile> request) {
+
+        if (request == null || request.isEmpty()) {
+            throw new ApiException(ErrorStatus._NOTHING_RECIPE_IMAGE);
+        }
+
+        return request;
     }
 
 }
