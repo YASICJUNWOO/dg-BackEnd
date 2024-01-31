551e4d609ae94d92f6769ae35f96b2c8f5596c63
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-02-01 02:09:57 +0900

    Fix: swagger Failed to fetch 오류 해결 (#86)
    - server url 설정
    
    Related: #85

--- src/main/java/com/example/dgbackend/global/config/SwaggerConfig.java
+++ src/main/java/com/example/dgbackend/global/config/SwaggerConfig.java
@@ -1,8 +1,8 @@
 package com.example.dgbackend.global.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,14 +12,14 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+            .addServersItem(new Server().url("/"))
+            .info(apiInfo());
     }
 
     private Info apiInfo() {
         return new Info()
-                .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
-                .version("1.0.0");
+            .title("음주미식회 Springdoc 테스트")
+            .description("Springdoc을 사용한 음주미식회 Swagger UI 테스트")
+            .version("1.0.0");
     }
 }
