c10fa075bd57b5b440ca627f89562a2a74af30c9
YASICJUNWOO <joonoo3@naver.com>
2024-02-05 20:31:42 +0900

    feat : RecipeRepository에서 page 기능 추가
    Related #90

--- src/main/java/com/example/dgbackend/domain/recipe/repository/RecipeRepository.java
+++ src/main/java/com/example/dgbackend/domain/recipe/repository/RecipeRepository.java
@@ -1,12 +1,14 @@
 package com.example.dgbackend.domain.recipe.repository;
 
 import com.example.dgbackend.domain.recipe.Recipe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    List<Recipe> findAllByState(boolean state);
+    Page<Recipe> findAllByState(boolean state, Pageable pageable);
     List<Recipe> findAllByNameAndMember_Name(String name, String memberName);
 }
