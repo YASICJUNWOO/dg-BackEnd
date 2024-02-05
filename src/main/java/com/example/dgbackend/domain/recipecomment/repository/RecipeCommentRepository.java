d5402b3ca0293576479b1022d92711cbcdcc0ec9
YASICJUNWOO <joonoo3@naver.com>
2024-02-05 20:38:07 +0900

    feat : RecipeCommentRepository 페이징 추가

--- src/main/java/com/example/dgbackend/domain/recipecomment/repository/RecipeCommentRepository.java
+++ src/main/java/com/example/dgbackend/domain/recipecomment/repository/RecipeCommentRepository.java
@@ -2,10 +2,12 @@ package com.example.dgbackend.domain.recipecomment.repository;
 
 import com.example.dgbackend.domain.recipe.Recipe;
 import com.example.dgbackend.domain.recipecomment.RecipeComment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface RecipeCommentRepository extends JpaRepository<RecipeComment, Long> {
-    List<RecipeComment> findAllByRecipe(Recipe recipe);
+    Page<RecipeComment> findAllByRecipe(Recipe recipe, Pageable pageable);
 }
