a138f38289a76a937a3c6a7bbcf5ceed33b86c02
YASICJUNWOO <joonoo3@naver.com>
2024-02-16 01:05:57 +0900

    refactor : HashTag Service 저장 로직 변경
    - 코드의 재사용을 위해 기존 HashTag를 파싱하여 저장하는 부분을 메서드 추출하였습니다.
    Related #112

--- src/main/java/com/example/dgbackend/domain/hashtag/service/HashTagCommandService.java
+++ src/main/java/com/example/dgbackend/domain/hashtag/service/HashTagCommandService.java
@@ -1,10 +1,13 @@
 package com.example.dgbackend.domain.hashtag.service;
 
 import com.example.dgbackend.domain.combination.Combination;
+import com.example.dgbackend.domain.hashtag.HashTag;
 
 import java.util.List;
 
 public interface HashTagCommandService {
 
     void uploadHashTag(Combination combination, List<String> hashTageNames);
+
+    List<HashTag> getHashTags(List<String> hashTageNames);
 }
