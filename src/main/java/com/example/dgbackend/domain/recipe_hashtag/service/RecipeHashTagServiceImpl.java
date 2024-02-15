6bc3d9dea62dbed71660c40e77c29c60e3fa7dd6
YASICJUNWOO <joonoo3@naver.com>
2024-02-16 01:45:30 +0900

    feat : RecipeHashTag 저장 로직
    Related #112

--- src/main/java/com/example/dgbackend/domain/recipe_hashtag/service/RecipeHashTagServiceImpl.java
+++ src/main/java/com/example/dgbackend/domain/recipe_hashtag/service/RecipeHashTagServiceImpl.java
@@ -1,13 +1,42 @@
 package com.example.dgbackend.domain.recipe_hashtag.service;
 
+import com.example.dgbackend.domain.hashtag.HashTag;
+import com.example.dgbackend.domain.hashtag.repository.HashTagRepository;
+import com.example.dgbackend.domain.hashtag.service.HashTagCommandServiceImpl;
+import com.example.dgbackend.domain.recipe.Recipe;
+import com.example.dgbackend.domain.recipe_hashtag.RecipeHashTag;
 import com.example.dgbackend.domain.recipe_hashtag.repository.RecipeHashTagRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class RecipeHashTagServiceImpl implements RecipeHashTagService{
+public class RecipeHashTagServiceImpl implements RecipeHashTagService {
 
     private final RecipeHashTagRepository recipeHashTagRepository;
+    private final HashTagCommandServiceImpl hashTagCommandService;
+    private final HashTagRepository hashTagRepository;
+
+    @Override
+    @Transactional
+    public List<RecipeHashTag> uploadRecipeHashTag(Recipe recipe, List<String> hashTagName) {
+
+        List<HashTag> hashTags = hashTagCommandService.getHashTags(hashTagName);
+
+        return hashTags.stream().map(hashTag -> {
+                    RecipeHashTag recipeHashTag = RecipeHashTag.builder()
+                            .recipe(recipe)
+                            .hashtag(hashTag)
+                            .build();
+
+                    return recipeHashTagRepository.save(recipeHashTag);
+                })
+                .toList();
+    }
 
 }
